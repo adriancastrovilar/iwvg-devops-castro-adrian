6491e10f8f82cb69db44922669c72efee6058aec
adriancastrovilar <adrian.castrov@alumnos.upm.es>
2023-09-23 18:26:07 +0200

    added search c #19

--- src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Searches.java
@@ -27,4 +27,11 @@ public class Searches {
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("No se encontraron resultados con el id proporcionado."));
     }
+
+    public Stream<Double> findDecimalFractionByUserName(String name) {
+        return new UsersDatabase().findAll()
+                .filter(user -> name.equals(user.getName()))
+                .flatMap(user -> user.getFractions().stream())
+                .map(Fraction::decimal);
+    }
 }
