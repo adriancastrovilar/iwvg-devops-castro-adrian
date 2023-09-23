9e1d8f03baf25d8a12d99d23570f930fa2fc3f41
adriancastrovilar <adrian.castrov@alumnos.upm.es>
2023-09-23 16:35:41 +0200

    Fixed Fraction #3

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -33,26 +33,6 @@ public class Fraction {
         this.denominator = denominator;
     }
 
-    public Fraction simplify() {
-        int gcd = gcd(this.numerator, this.denominator);
-
-        if (gcd == 0) {
-            this.numerator = 0;
-            this.denominator = 1;
-        } else {
-            this.numerator /= gcd;
-            this.denominator /= gcd;
-        }
-        return this;
-    }
-
-    private int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
-
     public Fraction() {
         this(1, 1);
     }
@@ -75,42 +55,6 @@ public class Fraction {
 
     public double decimal() {
         return (double) numerator / denominator;
-    }
-
-    public boolean isProper() {
-        return this.numerator < this.denominator;
-    }
-
-    public boolean isImproper() {
-        return this.numerator > this.denominator;
-    }
-
-    public boolean isEquivalent(Fraction fraction) {
-        return this.numerator * fraction.getDenominator() == this.denominator * fraction.getNumerator();
-    }
-
-    public Fraction add(Fraction fraction) {
-
-        int commonDenominator = this.denominator * fraction.denominator;
-
-        int adjustedNumerator1 = this.numerator * fraction.denominator;
-        int adjustedNumerator2 = fraction.numerator * this.denominator;
-
-        int resultNumerator = adjustedNumerator1 + adjustedNumerator2;
-
-        return new Fraction(resultNumerator, commonDenominator).simplify();
-    }
-
-    public Fraction multiply(Fraction fraction) {
-        int resultNumerator = this.numerator * fraction.numerator;
-        int resultDenominator = this.denominator * fraction.denominator;
-        return new Fraction(resultNumerator, resultDenominator).simplify();
-    }
-
-    public Fraction divide(Fraction fraction) {
-        int resultNumerator = this.numerator * fraction.denominator;
-        int resultDenominator = this.denominator * fraction.numerator;
-        return new Fraction(resultNumerator, resultDenominator).simplify();
     }
 
     @Override
